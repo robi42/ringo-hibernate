39116270ed7a60ad08b0ed120bb252aab0d0d9e0
Robert Thurnher <r.thurnher@gmail.com>
2010-04-01 03:14:45 +0200

    First sketch of testing.

--- test/all.js
+++ test/all.js
@@ -1,8 +1,27 @@
+// Run w/: $ ringo all
+
+include('io');
 include('ringo/unittest');
 var store = require('ringo/storage/hibernate');
 
-exports.testSomething = function () {
-    assertTrue(true);
+// Setup DB.
+var process = java.lang.Runtime.runtime.exec(['mysql', '-u', 'root', '-e',
+        'DROP DATABASE IF EXISTS ringotest; CREATE DATABASE ringotest;']);
+try {
+    process.waitFor();
+} finally {
+    new TextStream(new Stream(process.inputStream)).copy(system.stdout);
+    new TextStream(new Stream(process.errorStream)).copy(system.stderr);
+}
+
+exports.testSessionInit = function () {
+    assertNotNull(store.getSession());
+};
+
+exports.testBasicQuery = function () {
+    store.withSession(function (session) {
+        assertEqual(0, session.createCriteria('Person').list().size());
+    });
 };
 
 if (require.main == module.id) {
