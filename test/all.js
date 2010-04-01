4840170322f01b7597fd202419cb711786194f71
Robert Thurnher <r.thurnher@gmail.com>
2010-04-01 05:39:07 +0200

    Complete first sketch of CRUD testing.

--- test/all.js
+++ test/all.js
@@ -3,6 +3,10 @@
 include('io');
 include('ringo/unittest');
 var store = require('ringo/storage/hibernate');
+var person, Person = store.defineClass('Person');
+const FIRST_NAME_1 = 'Hans';
+const FIRST_NAME_2 = 'Herbert';
+const LAST_NAME = 'Wurst';
 
 // Setup DB.
 var process = java.lang.Runtime.runtime.exec(['mysql', '-u', 'root', '-e',
@@ -18,10 +22,34 @@ exports.testSessionInit = function () {
     assertNotNull(store.getSession());
 };
 
-exports.testBasicQuery = function () {
+exports.testPersistCreation = function () {
+    person = new Person();
+    person.firstName = FIRST_NAME_1;
+    person.lastName = LAST_NAME;
+    person.save();
+    person = Person.get(1);
+    assertNotNull(person);
+    assertEqual(FIRST_NAME_1, person.firstName);
+    assertEqual(LAST_NAME, person.lastName);
+};
+
+exports.testPersistUpdating = function () {
+    person = Person.all()[0];
+    assertNotNull(person);
+    person.firstName = FIRST_NAME_2;
+    person.save();
+    person = Person.get(1);
+    assertNotNull(person);
+    assertEqual(FIRST_NAME_2, person.firstName);
+    assertEqual(LAST_NAME, person.lastName);
+};
+
+exports.testBasicQuerying = function () {
     store.withSession(function (session) {
-        assertEqual(0, session.createCriteria('Person').list().size());
+        assertEqual(1, session.createCriteria('Person').list().size());
     });
+    assertEqual(1, Person.all().length);
+    assertEqual(LAST_NAME, Person.all()[0].lastName);
 };
 
 if (require.main == module.id) {
