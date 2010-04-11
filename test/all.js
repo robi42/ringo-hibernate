a0bab847ad5efd963453fd9d69753baba0a25851
Robert Thurnher <r.thurnher@gmail.com>
2010-04-12 01:12:41 +0200

    Refine mapping code formatting.

--- test/all.js
+++ test/all.js
@@ -5,14 +5,17 @@ addToClasspath('./config'); // To retrieve and load Hibernate config resources.
 var store = require('ringo/storage/hibernate');
 // Uncomment following line to test loading mappings from *.hbm.xml instead.
 //store.setHbmXmlDir(require('fs').join(module.directory, 'config'));
-var personId, person, // Define `Person` model w/ its O/R mapping.
-    Person = store.defineClass('Person', {table: 'persons', properties: {
+var personId, person; // Define `Person` model w/ its O/R mapping.
+var Person = store.defineClass('Person', {
+    table: 'persons',
+    properties: {
         firstName: {type: 'string', nullable: false},
         lastName: {type: 'string', nullable: false},
         birthDate: {type: 'timestamp', nullable: false},
         birthYear: {type: 'integer'},
         vitae: {column: 'resume', type: 'text', unique: true}
-}});
+    }
+});
 const FIRST_NAME_1 = 'Hans';
 const FIRST_NAME_2 = 'Herbert';
 const LAST_NAME = 'Wurst';
